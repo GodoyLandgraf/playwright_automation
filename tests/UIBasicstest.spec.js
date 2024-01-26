11d7f1de5b899c522abcac8e5ee705bf007cd3a1
root <root@DESKTOP-5J0VTVQ>
2024-01-26 12:29:28 -0300

    commit more tests

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -29,22 +29,10 @@ test('Login page Fail', async ({page})=>
     console.log (await page.locator(".card-body a").nth(1).textContent());
     const allTitles = await cardTitles.allTextContents();
     console.log(allTitles);
-
-
-
+    // nao pode pegar todo conteudo da página sem esperar ela carregar por inteira (exemplo certo no arquivo clienteApp)
 });
 
 
-test.only('Login Success all content', async ({page})=>
-{
-    await page.goto("https://rahulshettyacademy.com/client/");
-    await page.locator("#userEmail").fill("anshika@gmail.com");
-    await page.locator("#userPassword").fill("Iamking@000");
-    await page.locator("#login").click();
-    await page.waitForLoadState('networkidle');
-    const titles = await page.locator(".card-body b").allTextContents();
-    console.log(titles);
-});
 
 
 test('Login page Sucess', async ({page})=>
@@ -55,4 +43,44 @@ test('Login page Sucess', async ({page})=>
     await page.locator('#username').fill("rahulshettyacademy");
     await page.locator("[id='password']").fill("learning");
     await page.locator("[id='signInBtn']").click();
+});
+
+test('UI Controls', async ({page})=>
+{   
+    const documentLink = page.locator("[href*='documents-request']");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await page.locator('#username').fill("rahulshettyacademy");
+    await page.locator("[id='password']").fill("learning");
+    const dropdown = page.locator("select.form-control[data-style='btn-info']");
+    await dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    //assertion
+    await expect (page.locator(".radiotextsty").last()).toBeChecked();
+    await page.locator("#terms").click();
+    await expect (page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck();
+    await expect (page.locator("#terms")).not.toBeChecked();
+    await expect(documentLink).toHaveAttribute("class","blinkingText");
+    //somente printa se está checked
+    //console.log(page.locator(".radiotextsty").last()).isChecked;
+    //await page.pause();
+    //await page.pause();
+    await page.locator("[id='signInBtn']").click();
+});
+
+test.only('Child windows hadl', async ({browser})=>
+{   
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
+    const [newPage] = await Promise.all(
+    [context.waitForEvent('page'), 
+    documentLink.click(),] )
+    const text = await(newPage.locator(".red")).textContent();
+    const arrayText = text.split("@");
+    const domain = arrayText[1].split(" ")[0];
+    //console.log(domain);
+    await page.locator('#username').fill(domain);
 });
